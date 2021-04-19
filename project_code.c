8d1b024bac0011f8d265fdde6f015a0ebddc5ef5
mahmoud abdelhaleem <midomanolol180@gmail.com>
2021-04-19 03:37:57 +0200

    intial commit

---
+++ project_code.c
@@ -0,0 +1,18 @@
+struct student
+{
+   char name[50];
+   unsigned int ID;
+   unsigned int birth_year,birth_month,birth_day;
+   int score;
+};
+int main() {
+	printf("welcome in our simple data base ^_^");
+    //insert methods:
+    //linked list method:
+    //....
+    //....
+    //dynamic array method:
+    //....
+    //....
+	return 0;
+}
